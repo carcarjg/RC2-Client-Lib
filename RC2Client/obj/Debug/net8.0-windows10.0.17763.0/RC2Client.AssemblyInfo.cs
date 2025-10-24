6448a3ec68f1677c51204a7842a0572116460f90
Nyx <hello@rosesam.pw>
2025-10-24 15:04:27 -0500

    Fix

--- RC2Client/obj/Debug/net8.0-windows10.0.17763.0/RC2Client.AssemblyInfo.cs
+++ RC2Client/obj/Debug/net8.0-windows10.0.17763.0/RC2Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RC2Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1143efb099da14ef0e335b9402fba7bca6e3bcad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9f2396193437ba53e424da812f859c052c3a50")]
 [assembly: System.Reflection.AssemblyProductAttribute("RC2Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RC2Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
